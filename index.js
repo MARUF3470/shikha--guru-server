744243c68441d4045e31f13ee7c17aacbd9d6fec
MARUF3470 <smmaruf25@gmail.com>
2022-10-27 03:40:51 +0600

    data updated

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json')
-
+const subjects = require('./data/subjects.json')
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
-
-
-
+app.get('/subjects/:id', (req, res) => {
+    const id = req.params.id
+    // console.log(id)
+    const categorySubject = subjects.filter(subject => subject.id == id)
+    res.send(categorySubject)
+})
 app.listen(port, () => {
     console.log()
 })
